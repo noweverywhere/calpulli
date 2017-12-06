2830bc9aebf9d5861d5769bbfa5faabd3df499a7
noweverywhere <marinus@noweverywhere.ca>
2017-12-06 20:24:15 +1000

    Add postgres plugin to use UUIDs as primary ids

--- web/config/application.rb
+++ web/config/application.rb
@@ -27,5 +27,10 @@ module Src
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Make UUIDs the primary key
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
