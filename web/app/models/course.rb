a6660d05471802da494474710f6b57b61b654740
noweverywhere <marinus@noweverywhere.ca>
2017-12-15 22:30:27 +1000

    Validate courses to have valid 'year' property

--- web/app/models/course.rb
+++ web/app/models/course.rb
@@ -1,4 +1,9 @@
 class Course < ApplicationRecord
+  YEARS_COVERED_IN_CURRICULUM = %w(k1 k2 k3 p1 p2 p3 p4 p5 p6)
+
   has_and_belongs_to_many :course_licenses
   has_many :instructors, through: :course_licenses
+
+  validates :year, inclusion: { in: YEARS_COVERED_IN_CURRICULUM }
+
 end
