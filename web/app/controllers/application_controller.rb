ef2f83694a3b325ef645e8ae6ea1f118ec38de27
noweverywhere <marinus@noweverywhere.ca>
2017-12-15 10:34:39 +1000

    Redirect instructors with one courrse to #show on CoursesController

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Instructor)
+      if resource.courses.count == 1
+        course_path(resource.courses.first)
+       else
+        courses_path
+      end
+    end
+  end
 end
--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  authenticate :instructor do
+    resources :courses, only: [:index, :show]
+  end
   devise_for :instructors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
