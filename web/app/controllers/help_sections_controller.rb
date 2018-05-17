1cde6c4142c2d4bb9e04618e65a19de050693a53
noweverywhere <marinus@noweverywhere.ca>
2018-05-18 08:26:41 +1000

    Add help section

---
+++ web/app/controllers/help_sections_controller.rb
@@ -0,0 +1,21 @@
+class HelpSectionsController < ApplicationController
+  before_action :set_help_section, only: [:show]
+
+  def index
+    @help_sections = HelpSection.all
+  end
+
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_help_section
+      @help_section = HelpSection.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def help_section_params
+      params.require(:help_section).permit(:title, :body, :slug, :next, :previous)
+    end
+end
---
+++ web/spec/factories/help_sections.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :help_section do
+    title "MyString"
+    body "MyString"
+    slug "MyString"
+    next ""
+    previous ""
+  end
+end
